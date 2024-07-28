b71702ba37db16bad737f061216fcb1d31f12ad3
Infobest37 <147615831+Infobest37@users.noreply.github.com>
2024-07-28 20:06:10 +0300

    add n

--- Homework4/pythonProject/homework4.py
+++ Homework4/pythonProject/homework4.py
@@ -1,14 +1,15 @@
-#homework5.py
-immutable_var = ([1,2,4,5,6], 1, [7,8,9,10,11],2, "python", True)
-print( immutable_var)
-#Картеж не я вляется списком, в списке можно изменить объект, в картеже так делать нельзя, картедж так делать не может.
-#immutable_var("python")= "filipinu" - будет ошибка.
-immutable_var[2][0]=3 #в картеже можно создавать списки и в этих списках можно производить изменения присваевая каждому списку свой индекс
-print( immutable_var)
-mutable_lis = [1,2,3,4,5, "Python", "Juice", True]
-mutable_lis[0]=6
-print(mutable_lis)
-mutable_lis[6] = "apel"
-print(mutable_lis)
+#homework6.py
+my_dict = {"sist_1": "diagnostics", "sist_2": "calculation", "sist_3": "project"}
+print(my_dict)
+print(my_dict["sist_1"], my_dict.get("sist_4","Такого ключа нет"))
+my_dict.update({"sist_5": 12345, "sist_6": 6769})
+print(my_dict)
+my_dict.pop("sist_1")
+print(my_dict)
 
-
+my_set = {1, 2, 3, 4, 1, 2, 2, 4, 3, 2, 4}
+print(my_set)
+my_set.update({7,8})
+print(my_set)
+my_set.discard(7)
+print(my_set)
