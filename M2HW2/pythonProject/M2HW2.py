00142503b14d52678ac3b809d56a2c09c856d90b
Infobest37 <147615831+Infobest37@users.noreply.github.com>
2024-07-31 14:52:22 +0300

    add new1

--- M2HW2/pythonProject/M2HW2.py
+++ M2HW2/pythonProject/M2HW2.py
@@ -3,10 +3,13 @@ second = int(input("Введите любое число : "))
 third = int(input("Введите любое число : "))
 
 
-if first == second == first:
-    print(f"Совпали только 2 числа ")
-elif first == second or second == third or third == first:
+if first == second and second == third and third == first:
     print(f"Все три числа совпали")
-
+elif first != second and second == third or third == first:
+    print(f"Совпали только 2 числа ")
+elif first == second and second != third or third == first:
+    print(f"Совпали только 2 числа ")
+elif first == second or second == third and third != first:
+    print(f"Совпали только 2 числа ")
 else:
-    print(f"Ни одно число не совпало поэтому ваше число: ", 0)
+    print(f"Ни одно число не совпало поэтому ваше число: ")
