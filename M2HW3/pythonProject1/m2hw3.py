ec07e758873770f6994ddf81e2740bdb98049168
Infobest37 <147615831+Infobest37@users.noreply.github.com>
2024-08-01 18:24:39 +0300

    new

---
+++ M2HW3/pythonProject1/m2hw3.py
@@ -0,0 +1,9 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+nul = 0
+while nul < len(my_list):
+    if my_list[nul] < 0:
+      continue
+    if my_list[nul] == 0:
+        my_list.remove(my_list[nul])
+    print(my_list[nul])
+    nul += 1
