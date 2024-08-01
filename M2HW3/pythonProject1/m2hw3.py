33487deda4706aa655a1c4b58da1690ee552111f
Infobest37 <147615831+Infobest37@users.noreply.github.com>
2024-08-01 21:59:57 +0300

    new

--- M2HW3/pythonProject1/m2hw3.py
+++ M2HW3/pythonProject1/m2hw3.py
@@ -1,11 +1,11 @@
-my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+my_list = [1, 2, 3, 4, 5]
+a = 0
 nul = 0
+
 while nul < len(my_list):
-    if my_list[nul] < 0:
-        break
-    if my_list[nul] == 0:
-        my_list.remove(my_list[nul])
-
-    print(my_list[nul])
-
+    a += my_list[nul]
     nul += 1
+print(a)
+
+
+
