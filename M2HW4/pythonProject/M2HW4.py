95729ea9dcb2b74ee8818eee68120facb4ca016f
Infobest37 <147615831+Infobest37@users.noreply.github.com>
2024-08-02 18:02:27 +0300

    new

--- M2HW4/pythonProject/M2HW4.py
+++ M2HW4/pythonProject/M2HW4.py
@@ -1,25 +1,12 @@
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-primes = []
-not_primes = []
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+minus = []
+plus = []
+for i in list(numbers):
+    if i % 2 != 0:
+        minus.append(i)
 
-for i in numbers:
-    is_prime = True
-    if i > 1:
-        for j in range(2, i):
-            if i % j == 0:
-                is_prime = False
-                break
-        if is_prime:
-            primes.append(i)
+    else:
+        plus.append(i)
 
-for i in numbers:
-    is_prime = False
-    if i > 1:
-        for j in range(2, i):
-            if i % j == 0:
-                is_prime = True
-                break
-        if is_prime:
-            not_primes.append(i)
-print(f'primes =', primes)
-print(f'not_primes =', not_primes)
+print(minus)
+print(plus)
