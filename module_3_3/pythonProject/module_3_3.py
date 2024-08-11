d3d995a1cd7ad590258e69738bd64e333f8a1ea1
Infobest37 <147615831+Infobest37@users.noreply.github.com>
2024-08-11 09:57:27 +0300

    Merge remote-tracking branch 'origin/master'

--- module_3_3/pythonProject/module_3_3.py
+++ module_3_3/pythonProject/module_3_3.py
@@ -1,11 +1,18 @@
 def print_params(a = 1, b = 'строка', c = None):
     print(a, b, c)
 
-values_list = ["a,b,c,d,r"]
-values_dict = {"d": 1, "v": 5,"s": 7}
-values_list_2 = [4,67]
-print_params(values_list, values_dict)
+
+print_params(b = 25)
+print_params(c = [1,2,3])
+values_list = ["a,b,c", 1234, True]
+values_dict = {'a': 1, 'b': 5,'c': 7}
+values_list_2 = ["srt", 23]
+print_params(*values_list)
+print_params(**values_dict)
+print_params(*values_list_2, 42)
+
 def print_params1(**kyi):
     print(kyi)
 values_list_2 = {"a":4,"b":67}
 print_params1(**values_list_2)
+values_list_2 = [4,67]
