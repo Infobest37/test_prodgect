20c5be0166f71e6769645a46daae2802dc1e4420
Infobest37 <147615831+Infobest37@users.noreply.github.com>
2024-08-11 22:29:25 +0300

    Merge remote-tracking branch 'origin/master'

--- module_3_4/pythonProject1/module_3_4.py
+++ module_3_4/pythonProject1/module_3_4.py
@@ -1,1 +1,17 @@
-print("Hello World")
+def single_root_words(root_word,*other_words):
+    same_words = []
+
+    for word in other_words:
+        if root_word in word:
+
+            same_words.append(word)
+        elif word.upper() and word.lower() in root_word or word in root_word:
+             same_words.append(word)
+    return same_words
+
+
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result1)
+print(result2)
