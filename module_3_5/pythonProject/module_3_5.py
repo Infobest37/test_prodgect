6bff4fd7ce287cb62b6b7f410b44911f1edca6a5
Infobest37 <147615831+Infobest37@users.noreply.github.com>
2024-08-14 14:40:39 +0300

    Merge remote-tracking branch 'origin/master'

--- module_3_5/pythonProject/module_3_5.py
+++ module_3_5/pythonProject/module_3_5.py
@@ -1,13 +1,24 @@
+#
+# def get_multiplied_digits(number):
+#     str_number = str(number)
+#     if len(str_number) == 0:
+#         return 1
+#     first = int(str_number[0])
+#     if len(str_number) == 1:
+#         return first
+#     return first * get_multiplied_digits(int(str_number[1:]))
+#
+#
+# result = get_multiplied_digits(40203)
+# print(result)
+def summator(n):
+    str_n = str(n)
+    if len(str_n) == 0:
+        return 0
+    f = int(str_n[0])
+    if len(str_n) == 1:
+        return f
+    return f+summator(int(str_n[1:]))
 
-def get_multiplied_digits(number):
-    str_number = str(number)
-    if len(str_number) == 0:
-        return 1
-    first = int(str_number[0])
-    if len(str_number) == 1:
-        return first
-    return first * get_multiplied_digits(int(str_number[1:]))
 
-
-result = get_multiplied_digits(40203)
-print(result)
+print(summator(567))
---
+++ module_3_5/pythonProject1/module_3_6.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+window = tk.Tk()
+window.title("Калькулятор")
+window.geometry('400x200')
+window.resizable(False, False)
+window.mainloop()
