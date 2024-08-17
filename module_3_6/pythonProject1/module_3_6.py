3d3ad59dc8ffe2cfdd6f9cfe98b2d7cd2f896566
Infobest37 <147615831+Infobest37@users.noreply.github.com>
2024-08-17 14:14:19 +0300

    add

--- module_3_6/pythonProject1/module_3_6.py
+++ module_3_6/pythonProject1/module_3_6.py
@@ -66,19 +66,33 @@
 #
 
 
-
 def calculate_structure_sum(data_structure):
-  sum_1 = 0
-  if isinstance(data_structure, (int, float)):
-    sum_1 += data_structure
-  elif isinstance(data_structure, (list,tuple, set)):
-    for i in data_structure:
-      sum_1 += calculate_structure_sum(i)
-  elif isinstance(data_structure, dict):
-    for value in data_structure:
-      sum_1 += calculate_structure_sum(value)
+    sum_1 = 0
 
-  return sum_1
+    # Если элемент - число, просто добавляем его к сумме
+    if isinstance(data_structure, (int, float)):
+        sum_1 += data_structure
+
+    # Если элемент - строка, добавляем её длину к сумме
+    elif isinstance(data_structure, str):
+        sum_1 += len(data_structure)
+
+    # Если элемент - список, кортеж или множество, рекурсивно обрабатываем каждый элемент
+    elif isinstance(data_structure, (list, tuple, set)):
+        for item in data_structure:
+            sum_1 += calculate_structure_sum(item)
+
+    # Если элемент - словарь, обрабатываем его ключ и значение и плюсуем
+    elif isinstance(data_structure, dict):
+        for key, value in data_structure.items():
+            sum_1 += calculate_structure_sum(value)
+            sum_1 += calculate_structure_sum(key)
+
+    return sum_1
+
+
+# Пример использования
+
 data_structure = [
   [1, 2, 3],
   {'a': 4, 'b': 5},
@@ -88,3 +102,4 @@ data_structure = [
 ]
 result = calculate_structure_sum(data_structure)
 print(result)
+
