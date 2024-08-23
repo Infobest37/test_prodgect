ac227e991fab4d8feb300f0aa26b4cce5ed5d744
Infobest37 <147615831+Infobest37@users.noreply.github.com>
2024-08-23 14:58:23 +0300

    add

--- module_4_2/pythonProject/module_4_2.py
+++ module_4_2/pythonProject/module_4_2.py
@@ -1,12 +1,12 @@
 
-def test_function(b):
+def test_function():
     print("Привет я из пространства имен функции test_function")
 
-    def inner_function(b):
+    def inner_function():
         print("Я область видимости функции test_function")
 
-    inner_function(b)
+    inner_function()
     return inner_function
 
 
-print(inner_function)
+print(test_function())
