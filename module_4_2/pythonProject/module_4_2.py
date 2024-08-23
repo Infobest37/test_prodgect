4523e4f8f31d2498884253c5921497fe3df00039
Infobest37 <147615831+Infobest37@users.noreply.github.com>
2024-08-23 14:55:10 +0300

    add

--- module_4_2/pythonProject/module_4_2.py
+++ module_4_2/pythonProject/module_4_2.py
@@ -1,1 +1,12 @@
-print("Hello World")
+
+def test_function(b):
+    print("Привет я из пространства имен функции test_function")
+
+    def inner_function(b):
+        print("Я область видимости функции test_function")
+
+    inner_function(b)
+    return inner_function
+
+
+print(inner_function)
